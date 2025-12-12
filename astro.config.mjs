fb464be371cae3cdb61249f9165cefe404335e08
William Corner <w.corner@norwichuni.ac.uk>
2025-12-12 09:10:52 +0000

    Update config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     site: "https://ctlnorwich.github.io/introduction-to-web/",
+    base: "/introduction-to-web",
     integrations: [react()]
 });
