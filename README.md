48e7cd98cef6b2d6022186ddf09fe167adfe571d
William Corner <w.corner@norwichuni.ac.uk>
2026-01-08 15:37:41 +0000

    Add Starlight and start converting to mdx

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Introduction to Web
+
 New Introduction to Web content for 2026
+
+Starlight Version
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ctlnorwich.github.io/introduction-to-web/",
-    base: "/introduction-to-web",
-    integrations: [react()]
+    integrations: [starlight({
+        title: 'NUA Introduction to Web',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Introduction to HTML', slug: 'guides/introduction-to-html' },
+                ],
+            }
+        ],
+		}), react()],
 });
